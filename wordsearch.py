6d04ed7f894763d78ed1450fed26ce5ef11eaec5
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-02-26 11:54:30 -0500

    Added support for multiple search terms and defaults (so program will always run) TO FIX: *get_results: currently only compatible with older version; does not work with current version of hits

--- wordsearch.py
+++ wordsearch.py
@@ -1,18 +1,19 @@
 import sys
 import os
-#import fileinput
 
-"""
 
-CLASSES: 
-	- Scanner: 
-"""
+DEFAULT_FILE = 'htmlgrab.py'
+DEFAULT_SEARCH_TERMS = ['the','if']
 
 class Scanner:
 	"""
 	ATTRIBUTES:
-		- search_term: specified by the user
-		- hits: dictionary of lines and line number (key)
+		- search_term: specified by the user or default 
+		- hits:
+					hits = {
+				 			word: [(line number, line), (line number, line)],
+							word2: [(line number, line), (line number, line)]
+							}
 
 	METHODS:
 		- naive_traversal: provides a method for moving through a file linearly and 
@@ -22,49 +23,68 @@ class Scanner:
 
 	 """
 
-	def __init__(self,search_term):
+	def __init__(self,search_terms):
 		"""Create an instance of a scanner with an empty list of hits"""
-		self.search_term = search_term
-		self.hits = {}
+		self.search_terms = search_terms
 
 	def naive_traversal(self,fn,file_name = None):
 		"""Traverse a file, performing fn on each line"""
+		self.hits = {}
+		for term in self.search_terms:
+			self.hits[term] = [] # Initiates hits as an empty list for each search term
+
 		line_number = 1
 		with open(file_name,'r') as f:
 			for line in f:
-				fn(text=line,position=line_number,search_term= self.search_term)
+				fn(text=line,position=line_number,search_term=self.search_terms)
 				line_number +=1
+		print self.hits
 
 	def hit_check(self,text=None,search_term=None,position=None):
 		"""Check text for search_term in each line & store matches"""
-		words = text.split()
-		if search_term in words:
-			self.hits[position] = text.rstrip('\n')
+		
+		words_to_search = text.split()
+
+		for term in search_term:
+			if term in words_to_search:
+				self.hits[term].append((position,text))	
 
 	def get_results(self):
 		"""Print the search results"""
-		print "\r\n----Results----"
+		print "\r\n------------Results--------------"
 
 		if self.hits:
 			total_hits = len(self.hits)
-			print "Search term: %s"%self.search_term
+			print "Search term(s): %s"%self.search_terms
 			print "\r\nTotal hits: %s"%total_hits
 			
 			for key, value in self.hits.items():
-				print "    Line {0}: '{1}'".format(key,value)
-		else: 
-			print " '%s' does not appear in the document" %search_term
+				print "\t\tLine {0}: '{1}'".format(key,value)
+		else:
+			for term in self.search_terms: 
+				print "\n '%s' does not appear in the document." %term
 
 
 if __name__ == '__main__':
 	try: 
 		file_name = sys.argv[1]
-	except IndexError: 
-		print "Please enter a filename."
-	if os.path.exists(file_name):
-		search_term = raw_input("Search term:")
-		s = Scanner(search_term)
-		s.naive_traversal(s.hit_check, file_name = file_name)
-		s.get_results()
+	except IndexError: # No user input, try with default
+		file_name = DEFAULT_FILE
+		search_terms = DEFAULT_SEARCH_TERMS
 	else:
-		print "Please enter a valid filename"
+		if os.path.exists(file_name):
+			response = raw_input("Search term(s):")
+			search_terms = response.split(',') #returns an array
+		else:
+			file_name = DEFAULT_FILE
+			search_terms = DEFAULT_SEARCH_TERMS
+			print "No valid file name provided\nProceeding to search default file"
+
+	s = Scanner(search_terms)
+	s.naive_traversal(s.hit_check, file_name = file_name)
+	# s.get_results()
+		
+
+
+
+
