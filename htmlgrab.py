3f4cd20fb507512569d4e9809847bb7638eb78e4
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-02-26 11:18:41 -0500

    Separated methods for grabbing HTML, storing HTML and cleaning up the files generated TO DO: *write a real file_cleanup method (current is only placeholder) *add support for parsing the html

--- htmlgrab.py
+++ htmlgrab.py
@@ -1,31 +1,58 @@
 import urllib2
 import os
 import sys
-# import pdb
 
-default_urls = ['http://www.google.com','http://www.amazon.com','http://www.nytimes.com','http://www.racialicious.com','http://www.groupon.com','http://www.yelp.com']
+
+DEFAULT_URLS = ['http://www.google.com','http://www.amazon.com','http://www.nytimes.com','http://www.racialicious.com','http://www.groupon.com','http://www.yelp.com']
+
 current_directory = os.path.abspath(os.curdir)
 
-def html_grabber(urls = None,directory = None):
-	"""Grab all HTML from a page, write it to a new file in the specified directory (defaults to current directory)"""
-	if urls is None:
-		urls = default_urls
-	if directory is None:
-		directory = current_directory
+class HTMLFiler:
+	def __init__(self):
+		self.created_files = []
 
-	for url in urls:
-		site_name = url.split('www.')[1].rstrip('.com')
-		file_name = '{}/{}.html'.format(directory,site_name)
+	def html_grabber(self,urls = None):
+		"""Grab all HTML from a page, write it to a new file in the specified directory (defaults to current directory)"""
+		grabbed_info = []
 
-		new_file = open(file_name,'a') 
-		filish_object = urllib2.urlopen(url)
+		if urls is None:
+			urls = DEFAULT_URLS
 
-		data = filish_object.read()
-		new_file.write(data)
-		print "Site name: {} \n File: {}".format(site_name,file_name)
-		new_file.close()
+		for url in urls:
+			site_name = url.split('www.')[1].rstrip('.com')
+			try:
+				filish_object = urllib2.urlopen(url)
+			except urllib2.HTTPError as e:
+				print "Could not open file: {}".format(e.errno,e.strerror)
+			else:
+				data = filish_object.read()
+				grabbed_info.append((site_name, data))
+		return grabbed_info
+		
+
+	def html_storer(self,info,directory = None):
+		"""Store the data in a file """
+		if directory is None:
+			directory = current_directory
+		for site_name,data in info:
+			file_name = '{}/{}.html'.format(directory,site_name)
+
+			new_file = open(file_name,'a')
+			new_file.write(data)
+			print "Site name: {} \n File: {}".format(site_name,file_name)
+			new_file.close()
+
+			self.created_files.append(file_name)
+
+	def file_cleanup(self):
+		"""Remove all files associated with this session of the filer
+			Current: prints a list of all files created in this session"""
+		print self.created_files
+
 
 if __name__ == '__main__': 
-	html_grabber()
+	H = HTMLFiler()
+	info_to_be_stored = H.html_grabber()
+	H.html_storer(info_to_be_stored)
 
 
