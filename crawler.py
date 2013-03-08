e0ba62a85ee589f1f0710a072dbf0a035c33fbce
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-03-08 01:50:14 -0500

    Fixed some bugs and encountered some more (currently - duplicate key error on the first crawl

--- crawler.py
+++ crawler.py
@@ -1,7 +1,6 @@
 import sys
 import Queue
 import indexer
-import urlparse
 from pymongo import MongoClient,errors,DESCENDING,ASCENDING
 import pprint
 from threading import Thread
@@ -10,15 +9,16 @@ from urlparse import urlparse,urljoin
 
 DEFAULT_URLS = ['http://www.google.com','http://www.amazon.com','http://www.nytimes.com','http://www.racialicious.com','http://www.groupon.com','http://www.yelp.com']
 DEFAULT_DEPTH = 2
+RP = RobotFileParser()
 # SIMPLE_INDEX = MongoClient().index_db.simple_index # mongo collection
 
 
 class BFS_Crawler:
 	"""Create an instance of Crawler with a root and its tree"""
-	def __init__(self,start = ['http://www.smittenkitchen.com'],depth = DEFAULT_DEPTH):#add index = SIMPLE_INDEX as a param
+	def __init__(self,start = 'http://www.smittenkitchen.com',depth = DEFAULT_DEPTH):#add index = SIMPLE_INDEX as a param
 		"""Initialize the crawler with the starting urls"""
-		self.rp = RobotFileParser()
-		self.root = start
+		self.root = [url.strip() for url in start.split(',')]
+		print self.root
 		self.depth = depth
 		try:
 			self.simple_index = MongoClient().index_db.simple_index # mongo collection
@@ -26,7 +26,7 @@ class BFS_Crawler:
 			print "Your attempt to connect failed"
 
 	def make_mongo_index(self):
-		self.simple_index.create_index(('pages.url',ASCENDING),('_id',DESCENDING)) # creates an index on _id and pages.url (also allows queries for 'urls')
+		self.simple_index.create_index([('pages.url',DESCENDING),('_id',ASCENDING)]) # creates an index on _id and pages.url (also allows queries for 'urls')
 
 	def process_page(self,url): 
 		"""Retrieve all html data from a webpage,index it and return a list of links"""
@@ -43,9 +43,9 @@ class BFS_Crawler:
 		parsed_url = urlparse(url)
 		root = parsed_url[0] + '://' + page_url[1]
 		robots_url = urljoin(root,'/robots.txt')
-		self.rp.set_url(robot_url)
-		self.rp.read()
-		return self.rp.can_fetch("*",url)
+		RP.set_url(robot_url)
+		RP.read()
+		return RP.can_fetch("*",url)
 
 	def update_index(self,page):
 		new_entries = page.make_rlink_index() # all new entries to be added to the db
@@ -65,6 +65,8 @@ class BFS_Crawler:
 		self.ever_seen = set() #keeps track of which links have been crawled on this go (note: will re-crawl links already crawled in a new crawl)
 		
 		for link in self.root:
+			print "I'm about to put this into the Queue:"
+			print link
 			nodes.put((link,0))#enqueues (start,layer)
 			self.ever_seen.add(link)
 		self.make_mongo_index()#creates new index so that it's available for all subsequent queries (on '_id','url')
@@ -78,13 +80,14 @@ class BFS_Crawler:
 		print "You've done it"
 						
 	def crawler_process(self,queue):
-		print "New process started"
+		print "New process started. Current Queue:"
+		print queue
 
 		current_depth = 0
 		while True:
 			current_url, current_depth = queue.get()
 
-			print "Crawling: {0}\nCurrent Depth: {1}".format(current_url,current_depth)
+			print "Crawling: {0}\nCurrent Depth: {1}\n".format(current_url,current_depth)
 			
 			page = self.process_page(current_url) # # index the page, (maybe in try-catch)
 			if page:
@@ -100,7 +103,23 @@ class BFS_Crawler:
 
 
 if __name__ == '__main__':
-	c = BFS_Crawler()
+	"""Crawls sites either specified in the command line, at the prompt or as a default"""
+	try: 
+		sites_to_becrawled = sys.argv[1] #input must be a list
+		crawl_depth = sys.argv[2]
+	except IndexError: # No user input, try with default
+		confirmation = raw_input("Would you like to specify sites to crawl? y/n")
+		if confirmation in ['Y','y','Yes','yes']:
+			sites_to_becrawled = raw_input("Please give me a list of urls, separated by commas")
+
+			crawl_depth = raw_input("Please indicate a crawl depth:")
+		else: 
+			sites_to_becrawled = DEFAULT_URLS
+			crawl_depth = DEFAULT_DEPTH
+	print "I will crawl the following sites:"
+	print sites_to_becrawled
+	c = BFS_Crawler(start=sites_to_becrawled,depth=DEFAULT_DEPTH)
+	print "Starting crawl from %s"%str(sites_to_becrawled)
 	c.BFS_crawl()
 	
 
